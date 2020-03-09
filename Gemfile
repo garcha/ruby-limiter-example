0371f1b36d96fa21155ddc9c89fa33d724dab6fa
Jazz Garcha <jaspreet@garcha.com>
2020-03-09 16:40:32 -0700

    add redrate example

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 ruby "2.6.5"
 gem "ruby-limiter", require: "limiter"
+gem "redrate"
---
+++ redrate.rb
@@ -0,0 +1,23 @@
+require "bundler"
+Bundler.require(:default)
+
+COUNT = 50
+RATE = 5
+INTERVAL = 5 #seconds
+
+class CallCheck
+  extend Redrate::Mixin
+  limit_method :puts_hello, rate: RATE, interval: INTERVAL
+
+  def initialize
+    @tick = 0
+  end
+
+  def puts_hello(count = 1)
+    puts "Hello #{@tick}"
+    @tick += count
+  end
+end
+
+callcheck = CallCheck.new
+COUNT.times { callcheck.puts_hello }
